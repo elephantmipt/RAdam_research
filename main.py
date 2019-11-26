58a5e03703c4348a9fcc2e2ef0355728e62b233d
Nikita <Balaganskij.NN@phystech.edu>
2019-11-26 14:40:08 +0300

    hot fix

--- main.py
+++ main.py
@@ -30,11 +30,15 @@ def main():
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
     ])
     cifar_train = CIFAR10('.', train=True, transform=transform_train,
                           download=True)

d8dd716a61855bda55a2ef689d32857ab55ce569
artek.chumak <artek.chumak@gmail.com>
2019-11-26 14:47:56 +0300

    Fix sizes

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ def main():
         config = Config(**yaml.load(file)['opt_config'])
         scheduler_config = Config
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
+        # transforms.RandomCrop(32, padding=4),
+        # transforms.ToTensor(),
         transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         transforms.Resize((224, 224)),
         transforms.ToTensor()
